b9efafcbc67d8f92625f850cd4efa08f3c247599
Francisco Saroldi-Gonzalez <francisco.saroldi-gonzalez@mrjaffesclass.com>
2023-12-13 11:40:52 -0800

    start yahtzee

--- die6-master/Yahtzee.java
+++ die6-master/Yahtzee.java
@@ -1,4 +1,43 @@
 public class Yahtzee
 {
+    /**
+     * just make stuff into arrays, this is too jumbled
+     * 
+     * >:(
+     */
     
+    Die6 die1;
+    Die6 die2;
+    Die6 die3;
+    Die6 die4;
+    Die6 die5;
+    Die6 die6;
+    
+    /**
+     * Constructor
+     */
+    public Yahtzee()
+    {
+        die1 = new Die6();
+        die2 = new Die6();
+        die3 = new Die6();
+        die4 = new Die6();
+        die5 = new Die6();
+        die6 = new Die6();
+    }
+    
+    public void roll()
+    {
+        die1.roll();
+        die2.roll();
+        die3.roll();
+        die4.roll();
+        die5.roll();
+        die6.roll();
+    }
+    
+    public void rollDie()
+    {
+        
+    }
 }
